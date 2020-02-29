2470740b9ee9b34b71a0446403776295f0f42796
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-02-29 15:09:30 -0500

    eliminacion imports no utilizados

--- src/main/java/com/modeler/ModelerApp.java
+++ src/main/java/com/modeler/ModelerApp.java
@@ -2,14 +2,9 @@ package com.modeler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;/**
+/**
  * Hello world!
  *
  */
