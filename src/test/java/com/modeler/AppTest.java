ecd82859fe85d0108dbbad6b9a3f137d47715b9d
cvillamiljr <cesar.villamil@mail.escuelaing.edu.co>
2020-03-09 22:08:09 -0500

    pruebasUsuario

--- src/test/java/com/modeler/AppTest.java
+++ src/test/java/com/modeler/AppTest.java
@@ -10,8 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.modeler.model.Usuario;
 import com.modeler.repositories.UserRepository;
@@ -22,38 +23,66 @@ import com.modeler.repositories.UserRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(properties = {
-		"spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.H2Dialect",
-		"spring.datasource.url=jdbc:h2:mem:test",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=sa"
+        "spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.H2Dialect",
+        "spring.datasource.url=jdbc:h2:mem:test",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=sa"
 })
 @AutoConfigureMockMvc
 public class AppTest {
-	@Autowired
-	private UserRepository repo;
-	@Autowired
-	private MockMvc mock;
+    @Autowired
+    private UserRepository repo;
+    @Autowired
+    private MockMvc mock;
+
     @Test
-    public void testApp()
-    {
-    	
+    public void testApp() {
+
     }
-    /* Verificacion configuracion de base de datos
+
     @Test
     public void wouldBeRegisterAnUser() {
-    	repo.save(new Usuario("Juan@Diaz.com","juan","juan"));
-    	System.out.println(repo.findOne("Juan@Diaz.com").getCorreo());
-    	assertTrue(true);
+        try {
+
+            String v = mapper.writeValueAsString(new Usuario("cesarvilla@hotmail.com", "cesar", "123"));
+            mock.perform(post("/user").content(v).contentType("application/json")).andExpect(status().is2xxSuccessful());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
-    */
-    /* Example use
+
     @Test
-    public void wouldBeFindJay() {
-    	try {
-			mock.perform(get("/api")).andExpect(content().string("Hola"));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }*/
+    public void wouldNotBeRegisterAnUserXUserName() {
+
+        try {
+            repo.save(new Usuario("jeissonsa@hotmail.com", "JaySa", "jay123"));
+            String v = mapper.writeValueAsString(new Usuario("cesarvillac@hotmail.com", "JaySa", "123"));
+            mock.perform(post("/user").content(v).contentType("application/json")).andExpect(status().isBadRequest());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void wouldNotBeRegisterAnUserXEmail() {
+        try {
+            repo.save(new Usuario("js@hotmail.com", "JaySan", "jay123"));
+            String v = mapper.writeValueAsString(new Usuario("js@hotmail.com", "JaySa1", "123"));
+            mock.perform(post("/user").content(v).contentType("application/json")).andExpect(status().isBadRequest());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void wouldBeConsultAnUser() {
+        try {
+            repo.save(new Usuario("cv@hotmail.com", "CeVi", "jay123"));
+            String v = mapper.writeValueAsString(new JwtRequest("cv@hotmail.com", "jay123"));
+            mock.perform(get("/user/login").content(v).contentType("application/json")).andExpect(status().is2xxSuccessful());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
