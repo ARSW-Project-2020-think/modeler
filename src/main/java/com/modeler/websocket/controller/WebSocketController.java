a49f65700187396d3623bca05feb4abebcd9f074
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-04-01 18:14:45 -0500

    ap

--- src/main/java/com/modeler/websocket/controller/WebSocketController.java
+++ src/main/java/com/modeler/websocket/controller/WebSocketController.java
@@ -5,9 +5,6 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import com.modeler.exceptions.ModelerException;
 import com.modeler.model.Rectangulo;
 import com.modeler.services.RectangleServices;
 
@@ -19,15 +16,11 @@ public class WebSocketController {
 	private RectangleServices services;
 	
 	@MessageMapping("/rectangulo/modelo.{id}")
-	public void add(Rectangulo rectangulo,@DestinationVariable int id) {
-		try {
+	public void add(Rectangulo rectangulo,@DestinationVariable String id) {
 			System.out.println("nuevo rectangulo recibido");
-			services.save(rectangulo);
+			//services.save(rectangulo);
 			ms.convertAndSend("/shape/rectangle/modelo."+id,rectangulo);
 			System.out.println("entro");
-		} catch (ModelerException e) {
-			System.out.println("Error");
-		}
 	}
 	/**
 	@MessageMapping("/rectangulo/modelo.{id}/update")
