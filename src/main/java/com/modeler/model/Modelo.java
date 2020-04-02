afce345baacdabf53078f221c996ab69ac9aefe1
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-04-02 17:42:01 -0500

    app

--- src/main/java/com/modeler/model/Modelo.java
+++ src/main/java/com/modeler/model/Modelo.java
@@ -2,6 +2,7 @@ package com.modeler.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -32,7 +33,7 @@ public class Modelo {
 	private Version version;
 	
 	
-	@OneToMany(mappedBy="modelo")
+	@OneToMany(fetch=FetchType.EAGER,mappedBy="modelo",cascade=CascadeType.ALL)
 	private List<Rectangulo> rectangulos;
 	
 	@Column
