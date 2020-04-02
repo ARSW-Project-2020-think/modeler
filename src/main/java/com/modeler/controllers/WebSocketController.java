5ee76452d54379266a479aad6dd3b772a63d02a3
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-04-02 16:44:34 -0500

    app

--- src/main/java/com/modeler/controllers/WebSocketController.java
+++ src/main/java/com/modeler/controllers/WebSocketController.java
@@ -5,6 +5,7 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.modeler.exceptions.ModelerException;
 import com.modeler.model.Modelo;
@@ -13,21 +14,22 @@ import com.modeler.services.ModelServices;
 import com.modeler.services.RectangleServices;
 
 @Controller
+@CrossOrigin(origins="*")
 public class WebSocketController {
 	@Autowired
 	private SimpMessagingTemplate ms;
 	@Autowired
 	private ModelServices services;
 	
-	@MessageMapping("/rectangulo/modelo.{id}")
-	public void add(Rectangulo rectangulo,@DestinationVariable String id) {
+	@MessageMapping("/newrectangle.{idmodelo}")
+	public void add(Rectangulo rectangulo,@DestinationVariable int idmodelo) {
 			try {
 				System.out.println("nuevo rectangulo recibido "+rectangulo);
-				Modelo m = services.getModelById(Integer.parseInt(id));
+				Modelo m = services.getModelById(idmodelo);
 				Rectangulo r = new Rectangulo(rectangulo.getNombre(),rectangulo.getX(),rectangulo.getY(),rectangulo.getAncho(),rectangulo.getAlto());
 				m.addRectangulo(r);
 				services.save(m);
-				ms.convertAndSend("/shape/rectangulo/modelo."+id,r);
+				ms.convertAndSend("/shape/newrectangle."+idmodelo,r);
 			} catch (ModelerException e) {
 				System.out.println(">>>>>>>>>>>>>>><< Hubo un error "+e.getMessage());
 				e.printStackTrace();
