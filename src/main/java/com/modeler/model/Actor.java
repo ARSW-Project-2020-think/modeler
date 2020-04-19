33bc0b3f8f9d48dcd1d9482d366e46ce05f2ee47
JaySanchez0 <jeisson.sanchez@mail.escuelaing.edu.co>
2020-04-19 13:43:29 -0500

    app

--- src/main/java/com/modeler/model/Actor.java
+++ src/main/java/com/modeler/model/Actor.java
@@ -13,9 +13,13 @@ public class Actor extends Componente{
 	public Actor(String nombre, int x, int y,int ancho,int alto) {
 		super(nombre,x,y,ancho,alto);
 	}
+	public Actor(String nombre, int x, int y,int ancho,int alto,Modelo modelo) {
+		this(nombre,x,y,ancho,alto);
+		setModelo(modelo);
+	}
 	
 	@Override
 	public Object clone() {
-		return new Actor(super.getNombre(),super.getX(),super.getY(),super.getAncho(),super.getAlto());
+		return new Actor(super.getNombre(),super.getX(),super.getY(),super.getAncho(),super.getAlto(),super.getModelo());
 	}
 }
